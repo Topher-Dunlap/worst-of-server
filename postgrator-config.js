2aa08e08868d5b54545fa73de5c33bc3c24c81d2
topher-dunlap <thelonioustopher@yahoo.com>
2021-02-05 14:41:16 -0600

    changed some server and postgrator file

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
+const pg = require('pg'); pg.defaults.ssl = process.env.NODE_ENV === "production";
 
 module.exports = {
     "migrationsDirectory": "migrations",
     "driver": "pg",
-    "ssl": !!process.env.SSL,
+    // "ssl": !!process.env.SSL,
     "connectionString": (process.env.NODE_ENV === 'test')
         ? process.env.TEST_DATABASE_URL
         : process.env.DATABASE_URL,
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app')
 const knex = require('knex')
 const { PORT, DATABASE_URL } = require('./config')
-const pg = require("pg"); pg.defaults.ssl = true;
+const pg = require('pg'); pg.defaults.ssl = process.env.NODE_ENV === "production";
 
 const db = knex({
     client: 'pg',
