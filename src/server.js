cdb75c70150d52bfbeb5038eee77914e0e166174
topher-dunlap <thelonioustopher@yahoo.com>
2021-02-05 14:04:15 -0600

    making some changes to server.js for testing

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 const app = require('./app')
 const knex = require('knex')
 const { PORT, DATABASE_URL } = require('./config')
+const pg = require("pg"); pg.defaults.ssl = true;
 
 const db = knex({
-    client: 'pg',
+    // client: 'pg',
     connection: {
         connectionString: DATABASE_URL,
-        ssl: {
-            rejectedUnauthorized: false,
-        },
+        // ssl: {
+        //     rejectedUnauthorized: false,
+        // },
     },
 })
 
